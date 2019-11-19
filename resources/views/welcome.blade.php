c24dc2f9a846d14ac630bf9ed71891e2b80e12e1
Unknown <batinsimsek1998@gmail.com>
2019-11-19 16:25:06 +0100

    footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -106,16 +106,62 @@
 
     <div class="fourth-page">
         <div class="container">
-            <h3 class="flex-center mt-5">OFFERTEN</h3>
-        </div>
-    </div>
-
-    <div class="five-page">
-        <div class="container">
             <h3 class="flex-center mt-5">CONTACT US</h3>
         </div>
     </div>
 
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-6">
+                    <h6>Wie wij zijn</h6>
+                    <p class="text-justify">BarrocIntens.com <i>CODE WANTS TO BE SIMPLE </i> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores dicta fuga laboriosam non recusandae reprehenderit. Animi enim eveniet facere iste nemo odio omnis optio perferendis reiciendis repellat reprehenderit, similique?.</p>
+                </div>
+
+                <div class="col-xs-6 col-md-3">
+                    <h6>Pagina's</h6>
+                    <ul class="footer-links">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Sales</a></li>
+                        <li><a href="#">Maintenance</a></li>
+                        <li><a href="#">..</a></li>
+                        <li><a href="#">..</a></li>
+                        <li><a href="#">..</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-xs-6 col-md-3">
+                    <h6>Meer</h6>
+                    <ul class="footer-links">
+                        <li><a href="#">Over ons</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Bijdragen</a></li>
+                        <li><a href="#">Extra Info</a></li>
+                        <li><a href="#">Home Pagina</a></li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-sm-6 col-xs-12">
+                    <p class="copyright-text">Copyright &copy; 2019 All Rights Reserved by
+                        <a href="#">Barroc Intense</a>.
+                    </p>
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <ul class="social-icons">
+                        <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+                        <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
 
 </body>
 </html>
