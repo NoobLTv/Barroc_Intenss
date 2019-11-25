e42c651633b5f742ef0bb26a4fa8a4e3210c888c
Rik Jurgens <rikjurgens@outlook.com>
2019-11-25 09:43:18 +0100

    Mail changed

--- resources/views/emails/sale.blade.php
+++ resources/views/emails/sale.blade.php
@@ -1,7 +1,9 @@
 @component('mail::message')
 # Introduction
 
-Uw offerten is aangevraagd bij afdeling SALE
+Uw offerten is aangevraagd bij afdeling sales
+
+Er wordt spoedig contact met u opgenomen.
 
 @component('mail::button', ['url' => ''])
 Button Text
