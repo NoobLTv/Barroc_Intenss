eec5ae0b4cabcbd15835e7629d5bdbe1da6fc00e
Unknown <batinsimsek1998@gmail.com>
2019-11-29 14:35:50 +0100

    Search bar

--- app/Http/Controllers/suppliesController.php
+++ app/Http/Controllers/suppliesController.php
@@ -12,6 +12,33 @@ class suppliesController extends Controller
     {
         $this->middleware('auth');
     }
+
+    public function filter(Request $request){
+        $btn = $_POST['submitbtn'];
+        if($btn == "clear"){
+            $supplises = supplies::all();
+            return view('supplies.index', ['supplies' => $supplises]);
+        }
+
+        $name = $request->input('name');
+        $supplises = supplies::where('name')
+            ->orWhere('name', 'like', '%' . $name . '%')->get();
+
+        $checkbox_stock = $request->input('enough', false);
+        if($checkbox_stock == 'to-little'){
+            $supplises = supplies::where('available')
+                ->onWhere('available' , '>', 3)->get();
+        }
+
+        if($checkbox_stock == 'enough'){
+            $supplises = supplies::where('available')
+                ->orWhere('available', '>', 3)->get();
+        }
+
+        return view('supplies.index', ['supplies' => $supplises]);
+    }
+
+
     /**
      * Display a listing of the resource.
      *
@@ -20,8 +47,8 @@ class suppliesController extends Controller
     public function index()
     {
         //
-        $supplises = supplies::paginate(10);
-        return view('Supplies.index', ['supplises' => $supplises]);
+        $supplies = supplies::paginate(10);
+        return view('Supplies.index', ['supplies' => $supplies]);
     }
 
     /**
