646b995d1269c394ea0c6090d0722646fe857369
Esma Yilmaz <Esma_yilmaz7@hotmail.com>
2020-01-07 13:21:19 +0100

    Nu kan je niet zo maar op bijna alle scherm zonder in te loggen

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -31,4 +31,97 @@ class CheckRole
         return $next($request);
 
     }
+
+    public function finance($request, Closure $next)
+    {
+        $tooshame = '/';
+
+    if (Auth::check()){
+        $role_id = Auth::user()->role_id;
+        if ($role_id != 4){
+            return redirect($tooshame);
+        }
+    }
+    else{
+        return redirect($tooshame);
+    }
+
+    return $next($request);
+
+    }
+
+    public function customer($request, Closure $next)
+{
+    $tooshame = '/';
+
+    if (Auth::check()){
+        $role_id = Auth::user()->role_id;
+        if ($role_id != 5){
+            return redirect($tooshame);
+        }
+    }
+    else{
+        return redirect($tooshame);
+    }
+
+    return $next($request);
+
+}
+
+    public function supplies($request, Closure $next)
+    {
+        $tooshame = '/';
+
+        if (Auth::check()){
+            $role_id = Auth::user()->role_id;
+            if ($role_id != 1){
+                return redirect($tooshame);
+            }
+        }
+        else{
+            return redirect($tooshame);
+        }
+
+        return $next($request);
+
+    }
+
+    public function ceo($request, Closure $next)
+    {
+        $tooshame = '/';
+
+        if (Auth::check()){
+            $role_id = Auth::user()->role_id;
+            if ($role_id != 6){
+                return redirect($tooshame);
+            }
+        }
+        else{
+            return redirect($tooshame);
+        }
+
+        return $next($request);
+
+    }
+
+    public function maintenance($request, Closure $next)
+    {
+        $tooshame = '/';
+
+        if (Auth::check()){
+            $role_id = Auth::user()->role_id;
+            if ($role_id != 3){
+                return redirect($tooshame);
+            }
+        }
+        else{
+            return redirect($tooshame);
+        }
+
+        return $next($request);
+
+    }
+
+
+
 }
